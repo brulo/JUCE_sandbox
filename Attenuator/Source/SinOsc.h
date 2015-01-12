0bd1ef03c28dcd41c4adb1ebccf9004fc14f14c7
brulo <bruce.lott@gmail.com>
2015-01-11 17:26:14 -0800

    Initial SinOsc files

---
+++ Attenuator/Source/SinOsc.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    SinOsc.h
+    Created: 11 Jan 2015 5:24:05pm
+    Author:  Bruce Lott
+
+  ==============================================================================
+*/
+
+#ifndef SINOSC_H_INCLUDED
+#define SINOSC_H_INCLUDED
+
+
+
+
+
+#endif  // SINOSC_H_INCLUDED
